31c0d0671bf67089ce7a898288f5fdc7aa9b9857
drw <drw@users.noreply.github.com>
2019-10-11 12:10:48 -0400

    Add more login/logout URLs

--- urls.py
+++ urls.py
@@ -10,8 +10,10 @@ urlpatterns = [
 #    path('pdf/', views.IndexPrintView.as_view(), name='pdf'),
     #path('pdf/', pdf_view.make_pdf, name='pdf'),
     url(r'^login/', views.user_login, name='login'),
+    url(r'login/', views.user_login, name='login'),
     #url(r'^logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'), # Django 1.11,2+ version
     url(r'^logout/$', auth_views.logout, {'template_name': 'registration/logout.html'}, name='logout'), #Django 1.10 version
+    url(r'logout/$', auth_views.logout, {'template_name': 'registration/logout.html'}, name='logout'), #Django 1.10 version
     #path('login/', auth_views.LoginView.as_view(), name='login'),
     #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     #path('admin/', admin.site.urls),
