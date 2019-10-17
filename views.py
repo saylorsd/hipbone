2f9d4656b7ee340131e849baccee6b7286053c05
drw <drw@users.noreply.github.com>
2019-10-17 14:17:43 -0400

    Finish including error messages and refactoring lookups to split by parcel ID and address

--- views.py
+++ views.py
@@ -68,22 +68,35 @@ class IndexView(View):
                 cd = form.cleaned_data
                 address = cd['address']
                 url = "https://tools.wprdc.org/geo/geocode?addr={}".format(address)
-                response = requests.get(url)
+            else:
+                cd = form.cleaned_data
+                parcel_id = cd['address'] # [ ] Generalize the term 'address' to some include parcel ID
+                url = "https://tools.wprdc.org/geo/reverse_geocode/?pin={}".format(parcel_id)
+
+            response = requests.get(url)
+            if response.status_code != 200:
+                error_message = 'Look-up failed with status code {}.'.format(response.status_code)
+            else:
                 json_response = response.json()
-                if 'data' in json_response:
+                if 'data' in json_response: # Parse results of address lookup
                     json_data = json_response['data']
                     if 'parcel_id' in json_data:
                         self.parcel_id = json_data['parcel_id']
                         if 'regions' in json_data and 'us_census_tract' in json_data['regions']:
                             if 'name' in json_data['regions']['us_census_tract']:
                                 self.parcel_data['census_tract'] = json_data['regions']['us_census_tract']['name']
-            else:
-                self.msg = 'Search by parcel ID is not yet supported'
+                elif 'results' in json_response: # Parse results of parcel ID lookup
+                    json_results = json_response['results']
+                    self.parcel_id = parcel_id
+                    if 'us_census_tract' in json_results:
+                        if 'name' in json_results['us_census_tract']:
+                            self.parcel_data['census_tract'] = json_results['us_census_tract']['name']
 
         self.context = { 'address_form': form,
                 'parcel_id': self.parcel_id,
                 'parcel_data': self.parcel_data,
-                'msg': self.msg
+                'msg': self.msg,
+                'error_message': error_message
             }
         post_response = render(request, self.template_name, self.context)
         return post_response
