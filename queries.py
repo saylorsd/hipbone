a6cec55cee15f1e5b2b82124c22837a79545e529
drw <drwpub@gmail.com>
2019-11-05 19:25:55 +0000

    Factor out common parts of queries into standalone components

--- queries.py
+++ queries.py
@@ -1,6 +1,24 @@
 from django.db import connection, connections
 from icecream import ic
 
+BASE_QUERY = """SELECT ST_X(mm.geom_centroid) x_wgs84,
+    ST_Y(mm.geom_centroid) y_wgs84,
+    mm.parcel_year,
+    a1.prop_addr,
+    a1.prop_parcelnum,
+    a1.prop_legaldesc,
+    a2.quarter,
+    a2.vacant_percent,
+    a2.num_vacant,
+    a2.num_occupied
+    FROM parcel.master mm inner join parcel_admin_details a1 on mm.d3_id=a1.d3_id
+    LEFT JOIN vacancy a2 on mm.d3_id=a2.d3_id"""
+
+def append_date_clause(query, date_str):
+    if date_str is not None:
+        query += " AND '{}'::date BETWEEN start_date AND end_date\nAND a2.quarter = EXTRACT(Quarter FROM '{}'::date);".format(date_str, date_str)
+    return query
+
 def dictfetchall(cursor):
     """Return all rows from a cursor as a dict. Set up the cursor by executing
     the query and then run this to get the result as a list of dicts."""
@@ -11,48 +29,17 @@ def dictfetchall(cursor):
     ]
 
 def form_address_query(address, date_str=None):
-    query = """SELECT ST_X(mm.geom_centroid) x_wgs84,
-    ST_Y(mm.geom_centroid) y_wgs84,
-    mm.parcel_year,
-    a1.prop_addr,
-    a1.prop_parcelnum,
-    a1.prop_legaldesc,
-    a2.quarter,
-    a2.vacant_percent,
-    a2.num_vacant,
-    a2.num_occupied
-    FROM parcel.master mm inner join parcel_admin_details a1 on mm.d3_id=a1.d3_id
-    LEFT JOIN vacancy a2 on mm.d3_id=a2.d3_id"""
-
+    query = BASE_QUERY
     #--FUTURE TABLES LEFT JOIN HERE
     query += " WHERE prop_addr = UPPER('{}')".format(address)
-
-    if date_str is not None:
-        query += " AND '{}'::date BETWEEN start_date AND end_date\nAND a2.quarter = EXTRACT(Quarter FROM '{}'::date);".format(date_str, date_str)
-    return query
+    return append_date_clause(query, date_str)
 
 
 def form_parcel_query(parcel_number, date_str=None):
-    """The only difference between the two queries is the first part of the WHERE clause."""
-    query = """SELECT ST_X(mm.geom_centroid) x_wgs84,
-    ST_Y(mm.geom_centroid) y_wgs84,
-    mm.parcel_year,
-    a1.prop_addr,
-    a1.prop_parcelnum,
-    a1.prop_legaldesc,
-    a2.quarter,
-    a2.vacant_percent,
-    a2.num_vacant,
-    a2.num_occupied
-    FROM parcel.master mm inner join parcel_admin_details a1 on mm.d3_id=a1.d3_id
-    LEFT JOIN vacancy a2 on mm.d3_id=a2.d3_id"""
-
+    query = BASE_QUERY
     #--FUTURE TABLES LEFT JOIN HERE
     query += " WHERE prop_parcelnum = UPPER('{}')".format(parcel_number)
-
-    if date_str is not None:
-        query += " AND '{}'::date BETWEEN start_date AND end_date\nAND a2.quarter = EXTRACT(Quarter FROM '{}'::date);".format(date_str, date_str)
-    return query
+    return append_date_clause(query, date_str)
 
 def query_db(search_type, search_term, date_str='9/1/2019'):
     if search_type == 'address':
