1c02f59c49b71a213346fc8bfe3dd32bc61a3c36
drw <drwpub@gmail.com>
2019-12-03 18:39:27 +0000

    Add Census tract to query and display on front end

--- queries.py
+++ queries.py
@@ -15,12 +15,14 @@ BASE_QUERY = """SELECT ST_X(mm.geom_centroid) x_wgs84,
     ptv.last_sale_date,
     db.block_number,
     dbg.block_group_name,
-    c.city_name
+    c.city_name,
+    ct.census_tract_number
     FROM parcel.master mm inner join parcel_admin_details a1 on mm.d3_id=a1.d3_id
     LEFT JOIN vacancy a2 on mm.d3_id=a2.d3_id
     LEFT JOIN parcel_tax_and_values ptv on mm.d3_id=ptv.d3_id
     LEFT JOIN dim_block db on mm.block_id=db.block_id
     LEFT JOIN dim_block_group dbg on mm.block_group_id=dbg.block_group_id
+    LEFT JOIN dim_census_tract ct on mm.census_tract_id=ct.census_tract_id
     LEFT JOIN dim_city c on mm.city_id=c.city_id"""
 
 def append_date_clause(query, date_str):
